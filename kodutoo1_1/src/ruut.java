f9de81d00d9097265e034e5cd178f8a84e850e2d
Scannable-Hannibal <56818990+Scannable-Hannibal@users.noreply.github.com>
2020-04-15 23:13:02 +0300

    Update ruut.java
    Tegin parandused April 15, kell 23:12

--- kodutoo1_1/src/ruut.java
+++ kodutoo1_1/src/ruut.java
@@ -1,7 +1,12 @@
 public class ruut {
-    static void ruut(double r){
+    static void ruut_y(double r){
+        double d = 2*r;
+        double ans = 4*d;
+        System.out.printf("Ruudu ümbermõõt on: %.1f\n", ans);
+    }
+    static void ruut_p(double r){
         double d = 2*r;
         double ans = d*d;
-        System.out.printf("Ruudu ümbermõõt on: %.1f\n", ans);
+        System.out.printf("Ruudu pindala on: %.1f\n", ans);
     }
 }
